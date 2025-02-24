e121c37a86eb622c2e4a8842e472ee3c762c93f9
Your Name <you@example.com>
2025-02-24 23:58:20 +0300

    update unit

--- src/pagesComponent/units/Nav.jsx
+++ src/pagesComponent/units/Nav.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Routes } from 'react-router-dom'
 const Nav = () => {
   return (
     <nav className='flex items-center justify-between p-3 colorblue text-gray-300  sticky top-0'>
+    
         <NavLink className="hover:text-amber-700 text-lg" to="/">Home</NavLink>
         <div className='flex items-start justify-end gap-4 font-mono'>
             <NavLink className="hover:text-amber-700" to="/course/cats/pending">CAT/s</NavLink>
